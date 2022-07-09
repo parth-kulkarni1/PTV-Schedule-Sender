b8a69ad0393f1650a29d4228178a322c1acbdd43
Parth Kulkarni <parth.kulkarni2705@gmail.com>
2022-07-10 00:10:48 +1000

    concealed adderess and bing key ( made it an environment variable)

--- my_info.py
+++ my_info.py
@@ -1,10 +1,11 @@
 import datetime
 import time
+import os
 
-my_adderess = "7 Scherbourg Place, Hoppers Crossing, 3029, Victoria, Australia" # Enter your aderess in this variable
-my_bing_key = "AlfA8xF6WBgesbzwkw1nbf28l0joj2mbba-DagPz_bL1b5vArsuWMP7o6srflx_J" # Enter your basic bing maps key in this variable
+my_adderess = "7 ...., Hoppers Crossing, 3029, Victoria, Australia" # Enter your aderess in this variable
+my_bing_key = os.environ['my_bing_key'] # Enter your basic bing maps key in this variable
 my_route_type = [3] # 0 - Metro Trains, 1 - Tram, 2 - Bus, 3 - V/Line and Coach, 4 - NightRider
-my_suburb = ''
+my_suburb = 'Hoppers Crossing'
 my_day = 'Monday' # Make daytime objecs, so you can easily convert it into UTC Times. 
 
 my_day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday']
