2ca48e53b82a1f5d8a1523ca41f956ad5cea80b0
Parth-Kulkarni-Personal <parth.kulkarni2705@gmail.com>
2022-08-06 00:09:57 +1000

    Integrated a light python dictonary database. I chose to implement this as it will be easier to acess values, and they will stay persistent. Futhermore, i wanted to make the script as dynamic as possible, thus needed some way of saving user inputs.

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-import hmac
-import hashlib
+import hmac, hashlib
 import json
 import requests
 import geocoder
-import datetime
+import datetime, dateutil.parser
+import pytz
+import os
+from sqlitedict import SqliteDict
+        
 
-from my_info import my_adderess, my_bing_key, my_route_type, my_suburb     # Imports your adderess from the my_info.py file.
-from user_schedule import userSchedule
+BASE_URL = 'http://timetableapi.ptv.vic.gov.au'
+PTV_CLOCK_RESET = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month,datetime.datetime.now().day + 1, 10,0,0,0)
+devid = os.environ['DEV_ID_PTV']
+devkey = bytes(os.environ['DEV_KEY_PTV'], encoding='utf- 8')
+db = SqliteDict("user_station_preferences.sqlite", autocommit=True)
+
+my_adderess = "7 ...., Hoppers Crossing, 3029, Victoria, Australia" # Enter your aderess in this variable
+my_bing_key = "AlfA8xF6WBgesbzwkw1nbf28l0joj2mbba-DagPz_bL1b5vArsuWMP7o6srflx_J" # Enter your basic bing maps key in this variable
+my_route_type = [0,3] # 0 - Metro Trains, 1 - Tram, 2 - Bus, 3 - V/Line and Coach, 4 - NightRider
+my_suburb = 'Hoppers Crossing'
+
+
 
 def checkAPIStatusCode(requestObj):
-
     try:
         if requestObj.status_code != 200:
             raise Exception
@@ -18,54 +30,12 @@ def checkAPIStatusCode(requestObj):
         print("Error has occured with query. The API error code is", requestObj.status_code)
 
 
-def convertLocalTimeToUTC(user_local_time):
-
-
-    ptv_clock_reset = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month,datetime.datetime.now().day + 1, 10,0,0,0)
-
-    print(user_local_time,ptv_clock_reset)
-
-    
-    if user_local_time < ptv_clock_reset:
-        g = ((user_local_time - ptv_clock_reset))
-        time_utc = (str(g).split(",")[1].strip())
-        time_utc = time_utc.split(":")
-        new_time = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day, int(time_utc[0]),int(time_utc[1]),int(time_utc[2]))
-    
-    elif user_local_time >= ptv_clock_reset:
-        g = ((user_local_time - ptv_clock_reset))
-        time_utc = str(g).strip().split(":")
-        new_time = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day + 1, int(time_utc[0]), int(time_utc[1]), int(time_utc[2]))
-    
-
-    return new_time.isoformat()
-    
-
-    
-def convertUTCTimeToLocal(utctime_iso):
-
-    import dateutil.parser
-    import pytz
-    
-    utctime = dateutil.parser.parse(utctime_iso)
-
-    localtime = utctime.astimezone(pytz.timezone("Australia/Melbourne"))
-
-    return localtime
-
-
-
-
 def getAPI(apiRequest, devIdPara = '?'):
-
-    devid = 3002170
-    devkey = b'130e7f10-eadb-4236-808a-05e18250e0ec'
-    baseURL = 'http://timetableapi.ptv.vic.gov.au'
 
     updated_msg = '/v3/' + apiRequest + devIdPara + 'devid=' + str(devid)
 
     sig = hmac.new(devkey, updated_msg.encode('utf-8'),hashlib.sha1).hexdigest().upper()
-    url = baseURL  + updated_msg + '&signature=' + sig
+    url = BASE_URL  + updated_msg + '&signature=' + sig
 
     apiContent = requests.get(url)
 
@@ -73,116 +43,248 @@ def getAPI(apiRequest, devIdPara = '?'):
 
     json_obj = apiContent.json()
 
-    print(url)
-
-
     return json_obj
 
 
-def retriveStations_NearMe():
 
-    g = geocoder.bing(my_adderess,key = my_bing_key) # You change adderess to suburub .Returns the latitude and longititude of provided adderess in a list
+class ApiFunctions:
 
-    lattitude,longititude = g.latlng
-
-    default_max_radius = 15000 # This refers to the maximum radius from your location the API will look for retriving stop details. Change value as you please.
-
-    max_query_returns = 3
-
-    route_type_string= ''
-
-    for i in my_route_type:
-        route_type_string = route_type_string + 'route_types=' + str(i) + '&'
+    def __init__(self, user_local_time, utc_time_iso):
+        self.user_local_time = user_local_time
+        self.utc_time_iso = utc_time_iso
 
 
-    json_obj = getAPI(apiRequest='stops/location/' + str(lattitude) + ',' + str(longititude) + '?' + route_type_string + 'max_results=' + str(max_query_returns) + '&' 
-            + 'max_distance=' + str(default_max_radius), devIdPara='&')
+    def getCurrentDateString(self, reset = False, hour = 0, minutes = 0, seconds = 0):
 
-    
-    
+        if reset == False:
+            datetime_obj = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day + 0, hour, minutes, seconds)
 
-    setting_my_preferred_station  = {'stop_id': str(json_obj['stops'][0]['stop_id']), 'stop_name': str(json_obj['stops'][0]['stop_name']), 
-    'stop_suburb': str(json_obj['stops'][0]['stop_suburb']), 'route_type':str(json_obj['stops'][0]['route_type']), 'route_id': str(json_obj['stops'][0]['routes'][0]['route_id'])}
+        else:
+            datetime_obj = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day + 1, hour, minutes, seconds)
 
-    #print(setting_my_preferred_station)
+        return datetime_obj.isoformat()
 
-    return setting_my_preferred_station
+    def convertLocalTimeToUTC(self):
 
+        caluclated_string = self.user_local_time - PTV_CLOCK_RESET
 
+        
+        if self.user_local_time < PTV_CLOCK_RESET:
+            time_utc = (str(caluclated_string).split(",")[1].strip())
+            time_utc = time_utc.split(":")
+            api_utc_time = self.getCurrentDateString(reset=False, hour = int(time_utc[0]), minutes = int(time_utc[1]), seconds = int(time_utc[2]))
+        
+        else:
+            time_utc = str(caluclated_string).strip().split(":")
+            api_utc_time = self.getCurrentDateString(reset=True, hour = int(time_utc[0]), minutes = int(time_utc[1]), seconds = int(time_utc[2]))
+        
 
+        return api_utc_time
+        
 
-def getDirectionID():
+        
+    def convertUTCTimeToLocal(self):
 
-    station_info = retriveStations_NearMe()
+        utctime = dateutil.parser.parse(self.utc_time_iso)
 
-    json_obj = getAPI(apiRequest='directions/route/' + str(station_info['route_id']))
+        localtime = utctime.astimezone(pytz.timezone("Australia/Melbourne"))
 
-    directionID = json_obj['directions'][1]['direction_id']
-
-    return str(directionID)
-
-
-def outputDepartures(): # Need multipule run_ref's to be returned from this query from each depeature.
-
-    get_station_info = retriveStations_NearMe()
-    user_time = userSchedule()
-
-    json_obj = getAPI(apiRequest='departures/route_type/' + str(get_station_info['route_type']) + '/stop/'+ str(get_station_info['stop_id']) + 
-                      '/route/' + str(get_station_info['route_id'] )+ '?direction_id=' + getDirectionID() + '&date_utc=' + convertLocalTimeToUTC(user_time) + '&max_results=3'+
-                      '&include_geopath=false', devIdPara='&')
-    
-    run_refs = []
-
-    for i in range(len(json_obj['departures'])):
-        if json_obj['departures'][i]['scheduled_departure_utc'] >= convertLocalTimeToUTC(user_time):
-            run_refs.append(json_obj['departures'][i]['run_ref'])
-    
-    print(run_refs)
-    
-    return run_refs
-
-
-
-def getDepeatureSequence(json_obj,get_station_info):
-
-
-    for i in range(len(json_obj['departures'])):
-        if str(json_obj['departures'][i]['stop_id']) == get_station_info['stop_id']:
-            return json_obj['departures'][i]['departure_sequence'] 
+        return localtime
     
 
+    def route_string_builder(self):
+
+        route_type_string = ''
+
+        for i in my_route_type:
+            route_type_string = route_type_string + 'route_types=' + str(i) + '&'
+        
+
+        return route_type_string
 
 
-def outputStops():
+class UserStationInfo(ApiFunctions):
+
+    def __init__(self, my_adderess, my_bing_key):
+
+        self.my_adderess = my_adderess
+        self.my_bing_key = my_bing_key
+
+    
+    def get_coordinates(self):
+
+        g = geocoder.bing(self.my_adderess,key =self.my_bing_key) # You change adderess to suburub .Returns the latitude and longititude of provided adderess in a list
+
+        lattitude,longititude = g.latlng
+
+        return lattitude, longititude
+    
+
+        
+    def setPrefferedStation(self, json_obj):
+
+        '''Made a litle user input box, that will take the user's input and extract the information of their preffered station'''
+
+        station_info = None
+
+
+        if len(db) == 0:
+
+            user_stations_nearby = []
+
+            print("Pick a preffered Station please. Enter Y to proceed or N to quit. If you press N the closet Station will be chosen.\n")
+
+            for i in range(len(json_obj['stops'])):
+                user_stations_nearby.append(str(json_obj['stops'][i]['stop_name']).strip())
+
+            user_start_input = input()
+
+            if user_start_input.lower() == 'y':
+                print("Stations near you are:")
+                print(user_stations_nearby)
+
+                station_selected = input("Pick one of the following stations, by typing it's name with spaces and corrrect cases : ").strip()
+            
+                for i in range(len(json_obj['stops'])):
+                    if str(json_obj['stops'][i]['stop_name']).strip() == station_selected:
+                        print("Station Found")
+                        station_info = json_obj['stops'][i]
+                
+                print("Saving station information...")
+                
+                db["station"] = station_info
+
+            else:
+
+                nearest_station = json_obj['stops'][0]['stop_distance'] # Intalise the variable as the first element in list of Nearby Stations.
+
+
+                for i in range(len(json_obj['stops'])):
+                    if json_obj['stops'][i]['stop_distance'] <= nearest_station:
+                        station_info = json_obj['stops'][i]
+                
+                print("Saving station information...")
+
+                db["station"] = station_info
+
+        
+        else:
+            print("Station is already saved. You saved the following station:")
+            print()
+
+            for key, item in db.items():
+                    print("%s=%s" % (key, item))
+        
+
+        
+    def retriveStations_NearMe(self):
+
+        lattitude, longititude = self.get_coordinates()
+
+        DEFAULT_MAX_RADIUS = 15000 # This refers to the maximum radius from your location the API will look for retriving stop details. Change value as you please.
+        MAX_QUERY_RETURNS = 3 
+
+        json_obj = getAPI(apiRequest='stops/location/' + str(lattitude) + ',' + str(longititude) + '?' + super().route_string_builder() 
+                   + 'max_results=' + str(MAX_QUERY_RETURNS) + '&' + 'max_distance=' + str(DEFAULT_MAX_RADIUS), devIdPara='&')
+        
+        self.setPrefferedStation(json_obj)
+    
+
+    def getDirectionID(self): # We gonna have to print out all possible routes then, force user to pick a route, and store that within the db. Then we extract directonID
+
+        json_obj = getAPI(apiRequest='directions/route/' + str(db['station']['routes']['route_id']))
+
+        print(json_obj)
+
+'''
+
+class JourneyInfo(UserStationInfo, ApiFunctions):
+
+
+    def outputDepartures(): # Need multipule run_ref's to be returned from this query from each depeature.
 
         get_station_info = retriveStations_NearMe()
         user_time = userSchedule()
 
-        message_string = ''
+        json_obj = getAPI(apiRequest='departures/route_type/' + str(get_station_info['route_type']) + '/stop/'+ str(get_station_info['stop_id']) + 
+                        '/route/' + str(get_station_info['route_id'] )+ '?direction_id=' + getDirectionID() + '&date_utc=' + convertLocalTimeToUTC(user_time) + '&max_results=3'+
+                        '&include_geopath=false', devIdPara='&')
+        
+        run_refs = []
 
-        route_counter = 1
-
-        print(get_station_info['stop_name'])
-
-        for i in range(len(outputDepartures())):
-            json_obj = getAPI(apiRequest='pattern/run/' + outputDepartures()[i] + '/route_type/' + get_station_info['route_type'] + '?expand=All' 
-                            + '&date_utc=' + convertLocalTimeToUTC(user_time), devIdPara='&')    
+        for i in range(len(json_obj['departures'])):
+            if json_obj['departures'][i]['scheduled_departure_utc'] >= convertLocalTimeToUTC(user_time):
+                run_refs.append(json_obj['departures'][i]['run_ref'])
             
-            departure_sequence = getDepeatureSequence(json_obj,get_station_info)    
+        return run_refs
 
-            for j in range(departure_sequence - 1, len(json_obj['departures'])):
-                stop_id = (json_obj['departures'][j]['stop_id']) 
 
-                if str(json_obj['stops'][str(stop_id)]['stop_id']) == get_station_info['stop_id']:
-                    message_string = message_string + '\n' + 'Route ' + str(route_counter) + ' :'
-                    message_string = message_string + '\n' + '-----------'
-                    route_counter +=1
 
-                message_string = message_string + '\n' + (json_obj['stops'][str(stop_id)]['stop_name'] + ': ')
-                g = convertUTCTimeToLocal(json_obj['departures'][j]['scheduled_departure_utc'])
-                message_string = message_string + (str(g) + '\n')
+    def getDepeatureSequence(json_obj,get_station_info):
+
+
+        for i in range(len(json_obj['departures'])):
+            if str(json_obj['departures'][i]['stop_id']) == get_station_info['stop_id']:
+                return json_obj['departures'][i]['departure_sequence'] 
+
+
+    def outputStops():
+
+        with open("journey.txt",'w') as f:
+
+            get_station_info = retriveStations_NearMe()
+            user_time = userSchedule()
+
+            message_string = ''
+
+            route_counter = 1
+
+            print(get_station_info['stop_name'])
+
+
+
+            for i in range(len(outputDepartures())):
+                json_obj = getAPI(apiRequest='pattern/run/' + outputDepartures()[i] + '/route_type/' + get_station_info['route_type'] + '?expand=All' 
+                                    + '&date_utc=' + convertLocalTimeToUTC(user_time), devIdPara='&')    
                 
+                #print(json.dumps(json_obj, indent = 4))
+                    
+                departure_sequence = getDepeatureSequence(json_obj,get_station_info)  
 
-        return message_string
+                finish_sequence = finishSequence(json_obj)  
+
+                print("Finish sequence is", finish_sequence)
+
+                for j in range(departure_sequence - 1, int(finish_sequence)):
+                    stop_id = (json_obj['departures'][j]['stop_id']) 
+
+                    if str(json_obj['stops'][str(stop_id)]['stop_id']) == get_station_info['stop_id']:
+                        message_string = message_string + '\n' + 'Route ' + str(route_counter) + ' :'
+                        message_string = message_string + '\n' + '-----------'
+                        route_counter +=1
+
+                    message_string = message_string + '\n' + (json_obj['stops'][str(stop_id)]['stop_name'] + ': ')
+                    g = convertUTCTimeToLocal(json_obj['departures'][j]['scheduled_departure_utc'])
+                    message_string = message_string + (str(g) + '\n')
+                
+                
+            
+            f.write(message_string)
+
+            return message_string
 
 outputStops()
+
+'''
+
+def main():
+    UserStationInfo_info = UserStationInfo(my_adderess, my_bing_key)
+    json = UserStationInfo_info.retriveStations_NearMe()
+    print(json)
+
+    UserStationInfo_info.getDirectionID()
+
+
+
+if __name__ == "__main__":
+    main()
--- user_schedule.py
+++ user_schedule.py
@@ -1,47 +1,25 @@
 import datetime
 from datetime import date
 
-def calculateDay():
+class UserSchedule:
 
-    import calendar
-
-    return calendar.day_name[date.today().weekday()], datetime.datetime.now().weekday()
-
-
-def userSchedule():
-
-    userDays = {'Tuesday': '12:00 PM', 'Wednesday':'12:00 PM','Thursday':'8:00 AM'}
-
-    day_name, day_integer = calculateDay()
-
-    try:
-
-        if (day_name in userDays.keys()):
-
-                preffered_time = userDays[day_name]
-
-
-                in_time = datetime.datetime.strptime(preffered_time, "%I:%M %p")
-
-                out_time = datetime.datetime.strftime(in_time, "%H:%M")
-
-                out_time_split = out_time.split(":")
-
-                newdaytime = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month,datetime.datetime.now().day + 1,int(out_time_split[0]), 
-                                            int(out_time_split[1])).replace(microsecond=0)
-
-                return newdaytime
-
-        raise Exception
-
-    except:
-        print("Sorry I can't execute today. Add me in the schedule if you want me to work ;)")
-        exit(0)
-
-
-
+    def __init__(self, userDays):
+        self.userDays = userDays
     
-userSchedule()
+
+    def caluclateDay():
+        import calendar
+
+        return (calendar.day_name[datetime.date.today().weekday()]), (datetime.datetime.now().weekday())
+
+
+    def strip_time(self, day):
+
+        time_to_strip = self.userDays[day] 
+
+
+
+
 
 
 
